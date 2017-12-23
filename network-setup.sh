ab1ace955c421c22d8a4de5a6b7496209a1981b3
Kas Perch <the@nodebotani.st>
2017-12-23 13:17:48 -0600

    Basic stuff scripted

---
+++ network-setup.sh
@@ -0,0 +1,18 @@
+#! /bis/bash
+
+# Enable SSH
+sudo systemctl enable ssh
+
+# Enable Serial Login
+sudo echo "# Enable UART login" >> /boot/config.txt
+sudo echo "enable_uart=1" >> /boot/config.txt
+sudo echo "" >> /boot/config.txt
+
+# Add network to /etc/wpa_supplicant/wpa_supplicant.conf
+
+sudo echo "" >> /etc/wpa_supplicant/wpa_supplicant.conf
+sudo echo "network={" >> /etc/wpa_supplicant/wpa_supplicant.conf
+sudo echo "    ssid=\"NETWORK_ID\"" >> /etc/wpa_supplicant/wpa_supplicant.conf
+sudo echo "    psk=\"password\"" >> /etc/wpa_supplicant/wpa_supplicant.conf
+sudo echo "}" >> /etc/wpa_supplicant/wpa_supplicant.conf
+sudo echo "" >> /etc/wpa_supplicant/wpa_supplicant.conf
---
+++ user-setup.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# add nodebotanist user
+adduser nodebotanist
+
+# set password
+usermod -p $(echo ***** | openssl passwd -1 -stdin) nodebotanist
+
+# give nodebotanist sudoer privileges
+echo "nodebotanist    ALL=(ALL) ALL" >> /etc/sudoers
